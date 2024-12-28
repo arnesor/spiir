bd770663ef0e98be0cf71c4384b794378c248231
Arne Sørli <arnesor@users.noreply.github.com>
2024-12-28 10:09:01 +0100

    Refactor to functions

--- spiir/main.py
+++ spiir/main.py
@@ -1,7 +1,7 @@
-import numpy as np
+from decimal import Decimal
+
 import openpyxl
 import pandas as pd
-from decimal import Decimal
 from openpyxl.utils import get_column_letter
 from openpyxl.worksheet.dimensions import ColumnDimension, DimensionHolder
 
@@ -34,6 +34,48 @@ def format_spiir_sheet(filename: str) -> None:
         ws.cell(row=max_row + 1, column=col).value = sum_string
 
     wb.save(f"formatted-{year}.xlsx")
+
+
+def fix_split_transactions(df: pd.DataFrame) -> pd.DataFrame:
+    """Fix errors in split transactions.
+
+    There is a bug in Spiir which sometimes makes the original transaction in a
+    split transaction visible. It should be hidden. Solve this be removing the
+    original transaction (the first one) in each split group. Then add the non-split
+    transactions.
+
+    Args:
+        df: DataFrame with raw Spiir transaction data. Expected to have a column called
+            "SplitGroupId" which indicates split groups. Transactions with null values
+            in "SplitGroupId" are considered as non-split transactions.
+
+    Returns:
+        A DatFrame with the fixed list of transactions.
+    """
+    split_group_df = df.groupby("SplitGroupId", as_index=False, group_keys=False).apply(
+        lambda group: group.iloc[1:], include_groups=False
+    )
+    no_split_group_df = df[df.SplitGroupId.isnull()]
+    return pd.concat([split_group_df, no_split_group_df])
+
+
+def remove_excluded_and_extraordinary(df: pd.DataFrame) -> pd.DataFrame:
+    """Filter out excluded and extraordinary rows in the dataframe.
+
+    Args:
+        df: The dataframe that contains a "CategoryType" column to specify
+            category types and an "Extraordinary" column to indicate extraordinary entries.
+
+    Returns:
+        pd.DataFrame: A filtered dataframe excluding the "Exclude" and "Extraordinary" rows.
+    """
+    return df[(df["CategoryType"] != "Exclude") & (df["Extraordinary"] == False)]
+
+
+def correct_dates_by_year(df: pd.DataFrame, year: int) -> pd.DataFrame:
+    df = df.copy()  # Ensure the input DataFrame is not a slice
+    df["CorrectedDate"] = df["CustomDate"].combine_first(df["Date"])
+    return df[df["CorrectedDate"].dt.year == year]
 
 
 def main():
@@ -69,27 +111,13 @@ def main():
             "SplitGroupId": "string",
         },
     )
-#    df["Amount"] = df["Amount"].apply(Decimal)
-    df["Amount"] = df["Amount"].astype(float)
 
-    # There is a bug in Spiir which sometimes makes the original transaction in a
-    # split transaction visible. It should be hidden. Solve this be removing the
-    # original transaction (the first one) in each split group. Then add the non-split
-    # transactions.
-    split_group_df = df.groupby("SplitGroupId", as_index=False, group_keys=False).apply(
-        lambda group: group.iloc[1:],
-        include_groups = False
-    )
-    no_split_group_df = df[df.SplitGroupId.isnull()]
-    df2 = pd.concat([split_group_df, no_split_group_df])
-
-    df2 = df2[(df2["CategoryType"] != "Exclude") & (df2["Extraordinary"] == False)]
-    df2["CorrectedDate"] = df2["CustomDate"].where(
-        df2["CustomDate"].notnull(), df2["Date"]
-    )
-    df_year = df2[df2["CorrectedDate"].dt.year == year]
+    df_corrected = fix_split_transactions(df)
+    df_base = remove_excluded_and_extraordinary(df_corrected)
+    df_year = correct_dates_by_year(df_base, year)
     print(f"Shape after fixing: {df_year.shape}")
 
+    df_year["Amount"] = df["Amount"].apply(Decimal)
     category_table = pd.pivot_table(
         df_year,
         values="Amount",
@@ -100,10 +128,29 @@ def main():
     )
     category_table.columns = pd.to_datetime(category_table.columns).strftime("%b %Y")
 
+    # Convert Decimal columns to float
+    for col in category_table.columns:
+        category_table[col] = category_table[col].astype(float)
+
+    print("Detailed Description:")
+    print("Number of rows:", len(category_table))
+    print("Number of columns:", len(category_table.columns))
+    print("Column names:", category_table.columns.tolist())
+    print("Index type:", type(category_table.index))
+    print(
+        "Index values:",
+        (
+            category_table.index.tolist()
+            if len(category_table.index) < 200
+            else "Too many to display"
+        ),
+    )
+    print("Index name:", df.index.name)
+
     category_table.to_excel(out_filename)
     print("Finished writing spreadsheet.")
 
 
 if __name__ == "__main__":
     main()
-    format_spiir_sheet(out_filename)
+#    format_spiir_sheet(out_filename)
